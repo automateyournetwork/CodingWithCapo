5d32e270723e5d27eb96fa0296fa4af37cda0246
automateyournetwork <ptcapo@gmail.com>
2023-04-16 12:55:37 -0400

    added webex

--- chatgpt_client.py
+++ chatgpt_client.py
@@ -1,11 +1,16 @@
 import os
+import json
 import openai
+import requests
 from gtts import gTTS
 from dotenv import load_dotenv
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_KEY")
+webexToken = os.getenv("WEBEX_TOKEN")
+webexRoomId = os.getenv("WEBEX_ROOMID")
 
 question = input("What is your question for chatGPT? ")
 
@@ -18,18 +23,68 @@ def ask_question():
         ]
     )
 
-    result = ''
+    answer = ''
     for choice in response.choices:
-        result += choice.message.content
+        answer += choice.message.content
 
     print(f"We asked chatGPT { question } - here is their response: ")
-    print(result)
-    return(result)
+    print(answer)
+    return(answer)
 
-def create_mp3(result):
+def create_mp3(answer):
     language = 'en'
-    mp3 = gTTS(text = f"{ result }", lang=language)
+    mp3 = gTTS(text = f"{ answer }", lang=language)
     mp3.save(f'{ question }.mp3')
+
+def send_chat_to_webex(answer):
+    url = "https://webexapis.com/v1/messages"
+
+    payload = json.dumps({
+        "roomId": f" { webexRoomId }",
+        "text": f"We asked chatGPT { question }"
+    }
+    )
+
+    headers = {
+        "Authorization": f"Bearer { webexToken }",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(f"The Webex API call had a response code of: { response }")
+
+    payload = json.dumps({
+        "roomId": f" { webexRoomId }",
+        "text": f"Here was chatGPT's answer:\n{ answer } "
+    }
+    )
+
+    headers = {
+        "Authorization": f"Bearer { webexToken }",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(f"The Webex API call had a response code of: { response }")\
+
+def send_mp3_to_webex():
+    url = "https://webexapis.com/v1/messages"
+       
+    message_with_mp3 = MultipartEncoder(
+        {
+        "roomId": f"{ webexRoomId }",
+        "text": f"We asked chatGPT { question }",
+        "files": (
+            f"{ question }.mp3", open(f"{ question }.mp3", 'rb'),
+            'audio/mp3')
+        }
+    )
+
+    response = requests.post(url,data=message_with_mp3,headers={"Authorization": f"Bearer { webexToken }","Content-Type": f"{ message_with_mp3.content_type }"})
+    print(response)
 
 answer = ask_question()
 create_mp3(answer)
+if webexToken:
+    send_chat_to_webex(answer)
+    send_mp3_to_webex()
